1adaaa712a5d573cf3f92d74542cfbba673c2d4c
GBertonAR <gberton1967@gmail.com>
2023-09-17 12:13:02 -0300

    Avances en la estructura de las paginas, sumamos CSS

--- core_billetera/urls.py
+++ core_billetera/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mdp', views.mdp, name='mdp'),
+    path('promo', views.promo, name='promo'),
+    path('super', views.super, name='super'),
+    path('estadistica', views.estadistica, name='estadistica')
 
 ]
--- core_billetera/views.py
+++ core_billetera/views.py
@@ -11,3 +11,39 @@ def index(request):
         'es_instructor': True,
     }
     return render(request, "core/index.html", context)
+
+def estadistica(request):
+    usuario=getpass.getuser
+    context = {
+        'nombre_usuario': usuario,
+        'fecha': datetime.now(),
+        'es_instructor': True,
+    }
+    return render(request, "core/estadistica.html", context)
+
+def mdp(request):
+    usuario=getpass.getuser
+    context = {
+        'nombre_usuario': usuario,
+        'fecha': datetime.now(),
+        'es_instructor': True,
+    }
+    return render(request, "core/mdp.html", context)
+
+def promo(request):
+    usuario=getpass.getuser
+    context = {
+        'nombre_usuario': usuario,
+        'fecha': datetime.now(),
+        'es_instructor': True,
+    }
+    return render(request, "core/promo.html", context)
+
+def super(request):
+    usuario=getpass.getuser
+    context = {
+        'nombre_usuario': usuario,
+        'fecha': datetime.now(),
+        'es_instructor': True,
+    }
+    return render(request, "core/super.html", context)
